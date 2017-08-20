fb569618c8e8f9ae8a708b288f21466e16eaac01
Stanley Liu <stanleyl@symphonycommerce.com>
2017-08-20 09:39:39 -0700

    remove dll from config.js

--- internals/config.js
+++ internals/config.js
@@ -14,43 +14,44 @@ const ReactBoilerplate = {
    *
    * To disable the DLL Plugin, set this value to false.
    */
-  dllPlugin: {
-    defaults: {
-      /**
-       * we need to exclude dependencies which are not intended for the browser
-       * by listing them here.
-       */
-      exclude: [
-        'chalk',
-        'compression',
-        'cross-env',
-        'express',
-        'ip',
-        'minimist',
-        'sanitize.css',
-      ],
-
-      /**
-       * Specify any additional dependencies here. We include core-js and lodash
-       * since a lot of our dependencies depend on them and they get picked up by webpack.
-       */
-      include: ['core-js', 'eventsource-polyfill', 'babel-polyfill', 'lodash'],
-
-      // The path where the DLL manifest and bundle will get built
-      path: resolve('../node_modules/react-boilerplate-dlls'),
-    },
-
-    entry(pkg) {
-      const dependencyNames = Object.keys(pkg.dependencies);
-      const exclude = pkg.dllPlugin.exclude || ReactBoilerplate.dllPlugin.defaults.exclude;
-      const include = pkg.dllPlugin.include || ReactBoilerplate.dllPlugin.defaults.include;
-      const includeDependencies = uniq(dependencyNames.concat(include));
-
-      return {
-        reactBoilerplateDeps: pullAll(includeDependencies, exclude),
-      };
-    },
-  },
+   dllPlugin: false
+  // dllPlugin: {
+  //   defaults: {
+  //     /**
+  //      * we need to exclude dependencies which are not intended for the browser
+  //      * by listing them here.
+  //      */
+  //     exclude: [
+  //       'chalk',
+  //       'compression',
+  //       'cross-env',
+  //       'express',
+  //       'ip',
+  //       'minimist',
+  //       'sanitize.css',
+  //     ],
+  //
+  //     /**
+  //      * Specify any additional dependencies here. We include core-js and lodash
+  //      * since a lot of our dependencies depend on them and they get picked up by webpack.
+  //      */
+  //     include: ['core-js', 'eventsource-polyfill', 'babel-polyfill', 'lodash'],
+  //
+  //     // The path where the DLL manifest and bundle will get built
+  //     path: resolve('../node_modules/react-boilerplate-dlls'),
+  //   },
+  //
+  //   entry(pkg) {
+  //     const dependencyNames = Object.keys(pkg.dependencies);
+  //     const exclude = pkg.dllPlugin.exclude || ReactBoilerplate.dllPlugin.defaults.exclude;
+  //     const include = pkg.dllPlugin.include || ReactBoilerplate.dllPlugin.defaults.include;
+  //     const includeDependencies = uniq(dependencyNames.concat(include));
+  //
+  //     return {
+  //       reactBoilerplateDeps: pullAll(includeDependencies, exclude),
+  //     };
+  //   },
+  // },
 };
 
 module.exports = ReactBoilerplate;
